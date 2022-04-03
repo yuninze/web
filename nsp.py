6ec1ccd2d0c354c9d1e5aa3d3cb370c319ecb6bd
yuninze <gajeongsick@gmail.com>
2022-04-03 20:44:42 +0900

    nsp: usable

--- nsp.py
+++ nsp.py
@@ -1,12 +1,14 @@
 import numpy.random as nprnd
+import numpy as np
 import matplotlib.pyplot as plt
+from typing import (Iterable)
 from time import time as t
 from collections import Counter
 from wordcloud import WordCloud
 from konlpy.tag import Okt
 
 jvm="C:/Program Files/Java/jdk-18/bin/server/jvm.dll"
-font="C:/code/basic/GBR.ttf"
+font="C:/code/base/GBR.ttf"
 output_imgfile="wc.png"
 
 def gdstrb()->tuple:
@@ -20,23 +22,37 @@ def get_txt(txtfile=None)->str:
         #return str immediately
         return txtfile.read()
 
-def noun_freq(text,l_noun=2,n_noun=500,mtl=2.71)->dict:
+def l2s(i,l):
+    i=np.array(i,dtype=np.float64)
+    base=round(i.prod()**(1/l),5)
+    dist=np.sqrt((i-base)**2)
+    return dist
+
+def noun_freq(text,l_noun=2,n_noun=500,f_multiplier=1.5,norm=True)->dict:
     t0=t()
     okt=Okt(jvmpath=jvm)
     noun=okt.nouns(text)
-    #l_noun specifies len of noun
     noun=Counter([q for q in noun if len(q)>=l_noun])
-    f_noun={q:int(w*mtl) for q,w in noun.most_common(n_noun)}
-    print(f"done: elapsed in {t()-t0:.4f}s")
-    return f_noun
+    noun_map={q:w for q,w in noun.most_common(n_noun)}
+    if norm:
+        noun_map_len=len(noun)
+        noun_map_base=[noun_map[q] for q in noun_map.keys()]
+        noun_map_dist=l2s(noun_map_base,noun_map_len)
+        noun_f={q:w for q in noun_map.keys() for w in noun_map_dist}
+    try:
+        return noun_f
+    except:
+        return noun_map
+    finally:
+        print(f"done: elapsed in {t()-t0:.4f}s")
 
 def make_wcld(f_noun,output_imgfile=output_imgfile,font=font,
-    mask=None,size=(1920,1080),norm=False,img=False):
+    mask=None,size=(1920,1080),img=False):
     t0=t()
     mask=None#np.array(Image.open("image.png")
     wc=WordCloud(font_path=font,
         height=size[0],width=size[1],
-        mask=mask,normalize_plurals=norm,
+        mask=mask,normalize_plurals=False,
         max_words=10000)
     wc=wc.fit_words(f_noun)
     if img:
@@ -46,7 +62,7 @@ def make_wcld(f_noun,output_imgfile=output_imgfile,font=font,
     print(f"done: elasped in {t()-t0:.4f}s")
     plt.show()
 
-make_wcld(noun_freq(get_txt("ko.txt"),n_noun=200,mtl=3),img=True)
+make_wcld(noun_freq(get_txt("ko.txt"),n_noun=50,norm=True),img=True)
 
 def prac():
     return 0
