e07e2d5e395e9915ee9c24a0817b8074d38d7af2
yuninze <gajeongsick@gmail.com>
2022-04-24 20:36:37 +0900

    study

--- libvis.py
+++ libvis.py
@@ -2,64 +2,65 @@ import time as t
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from regex import E
 import seaborn as sns
 
 rg=np.random.default_rng(9405)
 xy=pd.DataFrame(rg.gamma(100,size=(1000,10)),columns=[q for q in range(10)])
+sns.set_theme("white")
 
-def pre_req():
-    fig,ax=plt.subplots(figsize=(5,8))
-    sns.set_theme("white")
+def bp(q,w,e,title="title"):
+    fg,ax=plt.subplots(figsize=(12,10))
+    #plt object
+    sns.boxplot(data=q,
+        x=w,
+        y=e)
+    plt.xlabel(f"{w}")
+    plt.ylabel(f"{e}")
+    plt.title(f"{title}")
+    return f"bp: shown"
 
-def quick_visual_bp(data,x,y):
-    sns.boxplot(data=data,x="col0",y="col1")
-    plt.xlabel(f"{col0.name}")
-    plt.ylabel(f"{col1.name}")
-    plt.title(f"{p}")
-    ax.set(xticklabels=["col0Explanation","col1Explanation"])
+def pp(w,e,title="title",a=10,figsize=(12,10)):
+    #startup setting
+    fg,ax=plt.subplots(figsize=figsize)
+    #have area factor by target variable data
+    target_var_area=e*a
+    while target_var_area.mean()<np.prod(figsize)*.1:
+        target_var_area*=.03
+    c=rg.random(e.shape[0])
+    #plt object
+    plt.scatter(x=w,
+        y=e,
+        s=target_var_area,
+        c=c,
+        alpha=0.5)
+    plt.xlabel(f"{w.name}")
+    plt.ylabel(f"{e.name}")
+    plt.title(f"{title}")
+    return f"pp: shown"
 
-###Basic visualizing
+def hm(q,title="title",figsize=(12,10),minmax=(None,None),fmt=".2f"):
+    #startup setting
+    fg,ax=plt.subplots(figsize=figsize)
+    #have corr df
+    q_corr=q.corr()
+    mask=np.triu(np.ones_like(q_corr,dtype=bool))
+    cmap=sns.diverging_palette(4,155,as_cmap=True)
+    sns.set_theme(style="whitegrid",font="monospace")
+    plt.xlabel(f"q.x")
+    plt.ylabel(f"q.y")
+    plt.title(f"{title}")
+    sns.heatmap(q_corr,
+        vmin=minmax[0],vmax=minmax[1],
+        mask=mask,cmap=cmap,
+        annot=True,center=0,square=True,linewidths=.5,fmt=fmt)
+    return f"hm: shown"
+
+###
+def sb(q,w,e,title="title"):
+    #innermost EDAs
+    sns.pairplot(xy,x_vars=[],y_vars=target_vars,size=4,aspect=1,kind="scatter")
+    plt.show(block=0)
+
 #for numeric data
 sns.pairplot(xy,hue="f{categorizable/indexable_data_label}",diag_kind="hist")
-#The innermost EDAs for
-sns.pairplot(xy,x_vars=[],y_vars=target_vars,size=4,aspect=1,kind="scatter")
-plt.show(block=0)
-
-
-#check data type is array-like
-type(xy)
-#get correlation
-xy_corr=xy.corr()
-#have mask, boolVec2int
-mask=np.triu(np.ones_like(xy_corr,dtype=bool))
-#
-sns.set_theme(style="whitegrid",font="monospace")
-#
-cmap=sns.diverging_palette(4,155,as_cmap=True)
-#cmap=sns.color_palette("light:#5A9",as_cmap=True)
-
-#
-sns.heatmap(xy_corr,mask=mask,cmap=cmap,annot=True,center=0,square=True,
-linewidths=.5,fmt=".3f",ax=ax)
-
-df.groupby('qtr').agg({"realgdp": ["mean",  "std"], "unemp": "mean"})
-fig, ax = plt.subplots(figsize=(5, 8))
-sns.boxplot(data=life_time_df, x='user_churn', y='max(life_time)')
-plt.xlabel('User Churn')
-plt.ylabel('Time Since Registration')
-plt.title('Time since registration and churn')
-ax.set(xticklabels=['Not Churned', 'Churned'])
-
-f,ax=plt.subplots(figsize=(13,9))
-x=pd.DataFrame(np.random.rand(10,3),columns=list("abc"))
-s=(20*np.random.rand(x.shape[0]))**2
-c=(np.random.rand(x.shape[0]))
-ax,fig=plt.subplots(figsize=(10,7))
-plt.scatter(x.a,x.b,s=s,c=c,alpha=0.5)
-
-#binning하고 대상 variable에 대해 scatter로. 
-ax.scatter(binnedData,targetVar)
-ax.tick_params(labelsize=14)
-ax.set_xlabel(binnedDataLabel,fontsize:int)
-ax.set_ylabel(targetVar,fontsize:int)
-linearly dependant?
--- sl_lin.py
+++ sl_lin.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 from time import time as t
+from scipy.stats import ttest_1samp,ttest_ind,chi2_contingency
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler,OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.feature_selection import SelectKBest,chi2,f_regression
 from sklearn.linear_model import LinearRegression,LogisticRegression
 from sklearn.model_selection import train_test_split
 
@@ -18,6 +20,53 @@ def captivate(seed=9405,type="gamma",size=1000):
         rg=seed.random(size=(size,4))
     return pd.DataFrame(rg,
         columns=list("abcd"))
+
+def have_rng(q,var_name,q_num=4):
+    if isinstance(q,(pd.DataFrame,pd.Series)):
+        q[f"{var_name}+rng"]=pd.qcut(var_name,q=q_num,labels=None)
+        return q
+    raise TypeError(f"{type(q)}")
+
+def sel_cat(q,idx):
+    return q.cat.categories[idx]
+
+def clarify(q,cat=False,w=None,m=None,rg=None):
+    if not cat:
+        if m:
+            r=ttest_1samp(q,popmean=m)
+            print(f"{r.pvalues:.10f}")
+            return r
+        else:
+            if not q.shape[0]==w.shape[0]:
+                row_cnt=min(q.shape[0],w.shape[0])
+                print(f"resampled: {row_cnt}")
+                q,w=[e.sample(n=row_cnt,random_state=rg) for e in (q,w)]
+                r=ttest_ind(q,w)
+            print(f"{r.pvalues:.20f}")
+            return r
+    r=chi2_contingency(q)
+    if isinstance(q,pd.Series):
+        name=q.name
+        return {"chi2":f"{r[0]:.10f}",
+            "p":f"{r[1]:.10f}",
+            "dof":f"{r[2]:.10f}",
+            "exp":pd.Series(r[3],
+            index=q.index.values,name=name)}
+    else:
+        name=q.columns.values
+        return {"chi2":f"{r[0]:.10f}",
+            "p":f"{r[1]:.10f}",
+            "dof":f"{r[2]:.10f}",
+            "exp":pd.DataFrame(r[3],
+            index=q.index.values,columns=name)}
+
+def sel_feature(q,w,m=chi2,f_num=1):
+    #chi2 for categorical features
+    #f_regression for continous features
+    if not q and w is None:
+        sk_cur=SelectKBest(score_func=m,k=f_num)
+        sk_cur.fit_transform(X=q,y=w)
+    raise NameError(f"no variables specified")
 
 def regress(data,y,ccols=None,type="ren"):
     #removable block
