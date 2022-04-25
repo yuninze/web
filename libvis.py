a220248a4a9cbbfa3b385da47159e2a50f7c716e
yuninze <gajeongsick@gmail.com>
2022-04-25 22:28:08 +0900

    .

--- libvis.py
+++ libvis.py
@@ -1,15 +1,12 @@
-import time as t
+from time import time as t
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-from regex import E
 import seaborn as sns
 
 rg=np.random.default_rng(9405)
-xy=pd.DataFrame(rg.gamma(100,size=(1000,10)),columns=[q for q in range(10)])
-sns.set_theme("white")
 
 def bp(q,w,e,title="title"):
+    t0=t()
     fg,ax=plt.subplots(figsize=(12,10))
     #plt object
     sns.boxplot(data=q,
@@ -18,9 +15,12 @@ def bp(q,w,e,title="title"):
     plt.xlabel(f"{w}")
     plt.ylabel(f"{e}")
     plt.title(f"{title}")
-    return f"bp: shown"
+    plt.show(block=False)
+    return f"bp: shown in {t()-t0:.3f}s"
 
-def pp(w,e,title="title",a=10,figsize=(12,10)):
+def sp(w,e,title="title",
+    a=10,figsize=(12,10)):
+    t0=t()
     #startup setting
     fg,ax=plt.subplots(figsize=figsize)
     #have area factor by target variable data
@@ -34,33 +34,49 @@ def pp(w,e,title="title",a=10,figsize=(12,10)):
         s=target_var_area,
         c=c,
         alpha=0.5)
-    plt.xlabel(f"{w.name}")
-    plt.ylabel(f"{e.name}")
-    plt.title(f"{title}")
-    return f"pp: shown"
+    plt.xlabel(w.name)
+    plt.ylabel(e.name)
+    plt.title(title)
+    plt.show(block=False)
+    return f"pp: shown in {t()-t0:.3f}s"
 
-def hm(q,title="title",figsize=(12,10),minmax=(None,None),fmt=".2f"):
+def hm(q,title="title",figsize=(12,10),
+    minmax=(None,None),fmt=".2f",
+    corner=""):
+    t0=t()
     #startup setting
     fg,ax=plt.subplots(figsize=figsize)
     #have corr df
     q_corr=q.corr()
-    mask=np.triu(np.ones_like(q_corr,dtype=bool))
+    if corner:
+        #boolVec to intVec
+        mask=np.triu(np.ones_like(q_corr,dtype=bool))
+    else:
+        mask=None
     cmap=sns.diverging_palette(4,155,as_cmap=True)
     sns.set_theme(style="whitegrid",font="monospace")
-    plt.xlabel(f"q.x")
-    plt.ylabel(f"q.y")
-    plt.title(f"{title}")
+    #plt object
     sns.heatmap(q_corr,
         vmin=minmax[0],vmax=minmax[1],
         mask=mask,cmap=cmap,
         annot=True,center=0,square=True,linewidths=.5,fmt=fmt)
-    return f"hm: shown"
+    plt.xlabel(f"q.x")
+    plt.ylabel(f"q.y")
+    plt.title(title)
+    plt.show(block=False)
+    return f"hm: shown in {t()-t0:.3f}s"
 
-###
-def sb(q,w,e,title="title"):
-    #innermost EDAs
-    sns.pairplot(xy,x_vars=[],y_vars=target_vars,size=4,aspect=1,kind="scatter")
-    plt.show(block=0)
-
-#for numeric data
-sns.pairplot(xy,hue="f{categorizable/indexable_data_label}",diag_kind="hist")
+def pp(q,r=None,w=None,e=None,
+    kind="scatter",diag_kind="hist",
+    hue=None,corner=True):
+    t0=t()
+    #basically EDA to numeric dtypes
+    #hue is categorical variable
+    sns.pairplot(data=q,
+        vars=r,
+        x_vars=w,y_vars=e,
+        kind=kind,diag_kind=diag_kind,
+        hue=hue,corner=corner)
+    #plt.{x,y}label goes to variable name or its list
+    plt.show(block=False)
+    return f"hm: shown in {t()-t0:.3f}s"
