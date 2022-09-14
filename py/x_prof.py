278ba5531e01dd47e3b7308efb7c91a9ecf3d1c1
yuninze <gajeongsick.gmail.com>
2022-09-14 18:12:11 +0900

    split

--- py/x_prof.py
+++
@@ -1,9 +0,0 @@
-import cProfile
-import pstats
-
-def prof(q):
-    with cProfile.Profile() as p:
-        q
-    stats=pstats.Stats(p)
-    stats.sort_stats(pstats.SortKey.TIME)
-    stats.print_stats()
