f74dbd53236ffd20a3b181bfc6345cbfaa811ef8
yinze <gajeongsick@gmail.com>
2024-02-13 23:19:33 +0900

    #

--- js.js
+++ js.js
@@ -1,36 +1,28 @@
-const http=require("node:http")
-const url=require("url")
-const fs=require("fs")
+const path=require("path")
+const fs=require("node:fs")
 
-const Util=require("./Util")
 const Stream=require("node-rtsp-stream")
 
-const assert=()=>{
-	return ". ".repeat(2)+Util.ima()
+const assert=(indent=0)=>{
+	let datetime=new Date()
+	datetimeString=datetime.toISOString().split(".")[0].replace("T",": ")
+	return " ".repeat(indent) + datetimeString
 }
 
-const hostname="172.30.1.18"
-const hostpath="http://"+hostname
-const port=80
-const mainPage="./main.html"
-const imageInErrorPage="./biLogo.png"
-
-const camData=JSON.parse(fs.readFileSync("../camera.json"))
-const cams=camData.camera.map(
-	(cam)=>{
+const camData=JSON.parse(fs.readFileSync("../camData.json"))
+const cam=camData.camera.map((cam)=>{
 		return [
 			cam.name,
 			camData.protocol+"://"+camData.account+"@"+camData.internAddress+cam.channel+camData.addressSuffix
 		]
-	}
-)
+	})
 
-const callStreams=()=>{
-	return cams.map((cam)=>{
+const Streaming=()=>{
+	return cam.map((camEach)=>{
 		return new Stream({
-			name:cam[0],
-			streamUrl:cam[1],
-			wsPort:90+cams.indexOf(cam),
+			name:camEach[0],
+			streamUrl:camEach[1],
+			wsPort:90+cam.indexOf(camEach),
 			ffmpegOptions:{
 				'-r':24,
 				'-loglevel':'warning',
@@ -40,137 +32,150 @@ const callStreams=()=>{
 	})
 }
 
-let streams=callStreams()
+let camStream=Streaming()
 setTimeout(()=>{
-	streams.map((stream)=>{
-		stream.stop()
-	})
-	streams=callStreams()
+	camStream.map((camStream)=>{camStream.stop()})
+	camStream=Streaming()
 },60000 * 10)
 
-const server=http.createServer(
-	(req,res)=>{
+const express=require("express")
+const favicon=require("serve-favicon")
+const rateLimit=require("express-rate-limit")
+const multer=require("multer")
+
+const server=express()
+const port=80
+const root="c:/code/web/"
+
+const limiter=rateLimit({
+	windowMs:1 * 60000,
+	max:100,
+	message:async(req,res)=>{
+		return sendHtmlString("It's done","Reached the rate limit")
+	}
+})
+
+const disk=multer.diskStorage({
+	destination:(req,file,cb)=>{
+		cb(null,"uploads/")
+	},
+	filename:(req,file,cb)=>{
+		cb(null,Date.now()+"_"+file.originalname)
+	}
+})
+const upload=multer({storage:disk})
+
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+server.use(express.static("./res"))
+server.use(limiter)
+server.use(favicon("./res/favicon.ico"))
+
+let filename
+let sessionInfo=new Array()
+let message={"method":null,"about":null}
+
+const nikki=(datetime,ip,ua,method,path)=>{
+	const nikkiStream=fs.createWriteStream(
+		"./nikki.csv",
+		{flags:"a"}
+	)
+	nikkiStream.write(
+		`${datetime},${ip},${ua},${method},${path}\n`
+	)
+}
+
+const messaging=(indent,message)=>{
+	const {method,about}=message
+	if (method=="LOW") {
+		msg=assert(indent) + ": " + `${method}: ${about}`
+	} else { 
+		msg=assert(indent) + ": " + `${sessionInfo.ip}: ${sessionInfo.ua}: ${method}: ${about}`
+	}
+	nikki(assert().replace(": ","T"),sessionInfo.ip,sessionInfo.ua,method,about)
+	console.log(msg)
+}
+
+const sendHtmlString=(title,content)=>{
+	const placeholderImage="error.png"
+	const mainPage="/"
+		return (`
+		<html>
+			<head>
+				<meta charset="utf-8">
+				<link rel="stylesheet" href="./style.css">
+				<title>${title}</title>
+			</head>
+			<body>
+				<div>
+					<img src=${placeholderImage} alt=${title} width="50%">
+					<h1>${title}</h1>
+					<h2>${content}</h2>
+					<a href=${mainPage}><h2>Go Back</h2></a>
+				</div>
+			</body>
+		</html>
+		`)
+}
+
+server.use((req,res,next)=>{
+	sessionInfo.ip=req.headers["x-forwarded-for"] || req.socket.remoteAddress
+	sessionInfo.ip=sessionInfo.ip.slice(sessionInfo.ip.indexOf(":",2)+1)
+	sessionInfo.ua=req.get("User-Agent")
+	
+	if (req.method=="GET") {
+		filename=req.originalUrl
+		message.method="GET"
+		message.about=filename
 		
-		const sendContent=(title,content)=>{
-			res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"})
-			res.write(`
-				<html>
-					<head>
-						<title>${title}</title>
-					</head>
-					<body>
-						<section>
-							<img src=${imageInErrorPage} alt=${title}>
-							<h1>${title}</h1>
-							<h4>${content}</h4>
-							<a href=${mainPage}><h1>Go Back</h1></a>
-						</section>
-					</body>
-				</html>
-			`,"utf-8")
-		}
-		
-		let browser=req.headers["user-agent"]?req.headers["user-agent"]:false
-		if (browser===false) {
-			console.log(`${assert()} ${req.socket.remoteAddress} (${browser}) => dropped`)
+		if (sessionInfo.ua==false || filename.length<3 || filename.endsWith("php")) {
+			message.about+=" (dropped)"
+			messaging(2,message)
+			res.set("content-type","text/html")
+			res.send(sendHtmlString("Something went wrong","Error"))
 			return res.end()
 		}
 		
-		const post=(url,data)=>{
-			
-			console.log(`${assert()}: POST: ${req.socket.remoteAddress}: ${browser}`)
-			
-			const dataString=JSON.stringify(data)
-			
-			const options={
-				method:"POST",
-				headers:{
-					"content-type":"application/json",
-					"content-length":dataString.length
-				},
-				timeout:1000,
-			}
-		
-			return new Promise((resolve,reject)=>{
-				const req=http.request(url,options,
-					(res)=>{
-						if (res.statusCode!=200) return reject(new Error("statusCode!=200"))
-						
-						let body=[]
-						res.on("data",(data)=>body.push(data))
-						res.on("end",()=>{
-							const resString=Buffer.concat(body).toString()
-							resolve(resString)
-						})
-					})
-				
-				req.on("error",(error)=>{
-					reject(error)
-				})
-				
-				req.on("timeout",(timeout)=>{
-					req.destory()
-					reject(new Error(assert() + "Timeout"))
-				})
-				
-				req.write(dataString)
-				req.end()
-			}) // promise
-		}
-		
-		if (req.method=="GET") {
-			let filePath
-			if (req.url.length>1) {
-				filePath="."+new URL(hostpath+req.url).pathname
-			} else {
-				filePath=mainPage
-			}
-			
-			console.log(`${assert()}: GET: ${req.socket.remoteAddress}: ${browser}: ${filePath}`)
-			
-			fs.readFile(filePath,
-				(error,data)=>{
-					if (error) {
-						console.log(assert()+error)
-						sendContent("Something went wrong",error.toString().replace("Error: ",""))
-						return res.end()
-					}
-					
-					let fileContentType
-					if (filePath.endsWith(".html")) {
-						fileContentType="text/html"
-					} else if (filePath.endsWith(".js")) {
-						fileContentType="text/javascript"
-					} else if (filePath.endsWith(".css")) {
-						fileContentType="text/css"
-					} else if (filePath.endsWith(".png")) {
-						fileContentType="image/png"
-					} else {
-						sendContent("Something went wrong","Wrong Content Type")
-						return res.end()
-					}
-					
-					res.writeHead(200,{
-						"Content-Type": fileContentType+"; charset=utf-8",
-					})
-					res.write(data,"utf-8")
-					return res.end()
-				}
-			)
-			
-			if (req.method=="POST") {
-				async ()=>{
-					const res=await post("http://172.30.1.18/sendSomething",data)
-				}
-			}
-			
-		}
-		
+	} else if (req.method="POST") {
+		message.method="POST"
+		message.about=req.originalUrl
+	
+	} else {
+		message.method="UNKNOWN"
+		message.about="*"
 	}
-)
+	
+	messaging(2,message)
+	next()
+})
 
-server.listen(port,hostname,
-	()=>{
-		console.log(`${assert()}: Run at ${hostname}:${port}`)
+server.get("*",(req,res)=>{
+	const reqPath=path.join(__dirname,filename)
+	if (fs.existsSync(reqPath)) {
+		res.sendFile(reqPath)
+	} else {
+		res.send(sendHtmlString(
+			"Something went wrong",
+			`'${filename}' does not exist`
+		))
+		return res.end()
 	}
-)
+})
+
+server.post("/upload",upload.single("file"),(req,res)=>{
+	if (req.file) {
+		const filename=req.file.filename
+		message.method="POST: UPLOAD"
+		message.about=filename
+		res.status(200).json({"success":true,"filename":filename})
+	} else if (!req.file) {
+		res.status(400).send(`This POST req. is likely null`)
+	}
+	messaging(3,message)
+})
+
+server.listen(port,()=>{
+	message.method="LOW"
+	message.about="..."
+	messaging(1,message)
+})
