9fd9a35d903ee66291c114dd913a7b6aa11c008a
yinze <gajeongsick@gmail.com>
2024-03-17 17:44:13 +0900

    add /getCount, /getColumns

--- js.js
+++ js.js
@@ -1,101 +1,38 @@
 const path=require("path")
 const fs=require("node:fs")
 const Papa=require("papaparse")
+
 const Stream=require("node-rtsp-stream")
-const camData=JSON.parse(fs.readFileSync("../camData.json"))
-const cam=camData.camera.map((cam)=>{
+const cams=JSON.parse(fs.readFileSync("../camData.json"))
+const cam=cams.cam.map(
+	(cam)=>{
 		return [
 			cam.name,
-			camData.protocol+"://"+camData.account+"@"+camData.internAddress+cam.channel+camData.addressSuffix
+			cams.protocol+"://"+cams.account+"@"+cams.internAddress+cam.channel+":554"+cams.addressSuffix
 		]
 	})
 
-const Streaming=()=>{
+const loadStreams=()=>{
 	return cam.map((camEach)=>{
 		return new Stream({
 			name:camEach[0],
 			streamUrl:camEach[1],
 			wsPort:90+cam.indexOf(camEach),
 			ffmpegOptions:{
-				'-r':24,
 				'-loglevel':'warning',
 				'-nostats':''
 			}
 		})
 	})
 }
-
-let camStream=Streaming()
-setTimeout(()=>{
-	message.method="RTSP"
-	message.about="Resetting"
-	camStream.map((camStream)=>{camStream.stop()})
-	camStream=Streaming()
-},60000 * 10)
-
-const assert=(indent=0)=>{
-	let datetime=new Date()
-	datetimeString=datetime.toISOString().split(".")[0].replace("T",": ")
-	return " ".repeat(indent) + datetimeString
-}
-
-const nikki=(datetime,ip,ua,method,path)=>{
-	const nikkiStream=fs.createWriteStream(
-		"./nikki.csv",
-		{flags:"a"}
-	)
-	nikkiStream.write(
-		`"${datetime}","${ip}","${ua}","${method}","${path}"\n`
-	)
-}
-
-const extractExt=(filenameString)=>{
-	const filenameStringBooleanVector=Array.from(filenameString).map(char=>char===".")
-	const extDotPosition=filenameStringBooleanVector.findLastIndex(vector=>vector===true)
-	return extDotPosition
-}
-
-let filename
-let sessionInfo=new Array()
-let message={"method":null,"about":null}
-const messaging=(indent,message)=>{
-	const {method,about}=message
-	if (method=="LOW") {
-		msg=assert(indent) + ": " + `${method}: ${about}`
-	} else { 
-		msg=assert(indent) + ": " + `${sessionInfo.ip}: ${sessionInfo.ua}: ${method}: ${about}`
-	}
-	nikki(assert().replace(": ","T"),sessionInfo.ip,sessionInfo.ua,method,about)
-	console.log(msg)
-}
-
-const sendHtmlString=(title,content)=>{
-	const placeholderImage="error.png"
-	const mainPage="/"
-		return (`
-		<html>
-			<head>
-				<meta charset="utf-8">
-				<link rel="stylesheet" href="./style.css">
-				<title>${title}</title>
-			</head>
-			<body>
-				<div>
-					<img src=${placeholderImage} alt=${title} width="50%">
-					<h1>${title}</h1>
-					<h2>${content}</h2>
-					<a href=${mainPage}><h2>Go Back</h2></a>
-				</div>
-			</body>
-		</html>
-		`)
-}
+const camStreams=loadStreams()
 
 const express=require("express")
 const favicon=require("serve-favicon")
 const server=express()
 const port=80
 const root="c:/code/web/"
+const rootPage="/"
 const uploadRoot="./upload/"
 
 const rateLimit=require("express-rate-limit")
@@ -106,7 +43,7 @@ const limiter=rateLimit({
 		message.method=req.method
 		message.about="BLOCKED"
 		messaging(3,message)
-		return sendHtmlString("It's done","Reached the rate limit")
+		return errorPage("It's done","Reached limit")
 	}
 })
 
@@ -132,6 +69,62 @@ const disk=multer.diskStorage({
 })
 const upload=multer({storage:disk})
 
+const assert=(indent=0)=>{
+	let datetime=new Date()
+	datetimeString=datetime.toISOString().split(".")[0].replace("T",": ")
+	return " ".repeat(indent) + datetimeString
+}
+
+const extractExt=(filenameString)=>{
+	const filenameStringBooleanVector=Array.from(filenameString).map(char=>char===".")
+	const extDotPosition=filenameStringBooleanVector.findLastIndex(vector=>vector===true)
+	return extDotPosition
+}
+
+const writeNikki=(datetime,ip,ua,method,path)=>{
+	const nikki=fs.createWriteStream(
+		"./nikki.csv",
+		{flags:"a"}
+	)
+	nikki.write(
+		`"${datetime}","${ip}","${ua}","${method}","${path}"\n`
+	)
+}
+
+let sessionInfo=new Array()
+let message={"method":null,"about":null}
+const messaging=(indent,message)=>{
+	const {method,about}=message
+	if (method=="LOW") {
+		msg=assert(indent) + ": " + `${method}: ${about}`
+	} else { 
+		msg=assert(indent) + ": " + `${sessionInfo.ip}: ${sessionInfo.ua}: ${method}: ${about}`
+	}
+	writeNikki(assert().replace(": ","T"),sessionInfo.ip,sessionInfo.ua,method,about)
+	console.log(msg)
+}
+
+const errorPage=(title,content)=>{
+	const placeholderImage="error.png"
+	return (`
+	<html>
+		<head>
+			<meta charset="utf-8">
+			<link rel="stylesheet" href="./style.css">
+			<title>${title}</title>
+		</head>
+		<body>
+			<div>
+				<img src=${placeholderImage} alt=${title} width="50%">
+				<h1>${title}</h1>
+				<h2>${content}</h2>
+				<a href=${rootPage}><h2>Go Back</h2></a>
+			</div>
+		</body>
+	</html>
+	`)
+}
+
 server.use(express.static("./res"))
 server.use(limiter)
 server.use(favicon("./res/favicon.ico"))
@@ -142,16 +135,14 @@ server.use((req,res,next)=>{
 	sessionInfo.ua=req.get("User-Agent")
 	
 	if (req.method=="GET") {
-		filename=req.originalUrl
 		message.method="GET"
-		message.about=filename
+		message.about=req.originalUrl
 		
-		if (sessionInfo.ua==false || filename.length<3 || filename.endsWith("php")) {
+		if (sessionInfo.ua==false || req.originalUrl.length<3 || req.originalUrl.endsWith("php")) {
 			message.about+=" (dropped)"
 			messaging(2,message)
-			res.set("content-type","text/html")
-			res.send(sendHtmlString("Something went wrong","Error"))
-			return res.end()
+			res.set("content-type","text/html").send(errorPage("Something went wrong","Error"))
+			res.end()
 		}
 		
 	} else if (req.method="POST") {
@@ -168,49 +159,64 @@ server.use((req,res,next)=>{
 })
 
 server.get("*",(req,res)=>{
-	const reqPath=path.join(__dirname,filename)
+	const reqPath=path.join(__dirname,req.originalUrl)
+	
 	if (fs.existsSync(reqPath)) {
 		res.sendFile(reqPath)
-	} else if (req.originalUrl==="/uploadProofCount") {
-		let files=new Promise((resolve,reject)=>{
-			return fs.readdir(uploadRoot,
-				(fail,ok)=>{
-					fail != null ? reject(fail) : resolve(ok)
-				}
-			)
-		})
-		files.then(f=>{
-			res.status(200)
-			res.json({content:f.length})
-		})
-	} else if (req.originalUrl==="/trainingIdx") {
-		const fp=fs.readFileSync("c:/code/x.arrow","utf8")
-		const training=Papa.parse(fp,{
-			delimiter:",",
-			header:true
-		})
-		const idx=[...new Set(training.data.map(d=>d.idx))]
-		res.status(200)
-		res.json({content:idx,len:idx.length})
-	}	else {
-		res.send(sendHtmlString(
-			"Something went wrong",
-			`'${filename}' does not exist`
-		))
+	
+	} else if (req.originalUrl==="/camStreams") {
+		res.set("content-type","text/html").send(errorPage("Went wrong","Not Implemented"))
 		res.end()
+			
+	} else if (req.originalUrl==="/count") {
+		let files=new Promise((resolve,reject)=>{
+			return fs.readdir(uploadRoot,(fail,ok)=>{
+				fail != null ? reject(fail) : resolve(ok)
+			})
+		})
+		
+		files.then(f=>{
+			res.status(200).json({
+				content:f.length
+			})
+		})
+	
+	} else if (req.originalUrl==="/csvData") {
+		const fp=fs.readFileSync("c:/code/f.csv","utf8")
+		const csvData=Papa.parse(fp,{
+			delimiter:",",
+			headers:false
+		})
+		
+		res.status(200).json({
+			data:{
+				columns:csvData.data[0],
+				len:csvData.data.length-1,
+				latestIndex:csvData.data[csvData.data.length-2][0]
+			}
+		})
+		
+	}	else {
+		res.send(errorPage("Went wrong",`An action for '${req.originalUrl}' does not exist`))
+		res.end()
+	
 	}
 })
 
 server.post("/uploadProof",upload.single("proofFile"),(req,res)=>{
 	message.method="POST: Uploading Attempting"
+	
 	if (req.file) {
 		message.about=`${req.file.filename} (${req.file.size})`
 		res.status(200).json({content:`Uploaded: ${(req.file.size/1024).toFixed(2)}kB`})
+		
 	} else {
 		message.about="There was an exception"
 		res.status(400)
+		
 	}
 	messaging(3,message)
+	
 })
 
 server.listen(port,()=>{
